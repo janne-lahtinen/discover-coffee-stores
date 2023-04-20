818b9fb9632e679d4583aa2d5447ad309f6b1ec7
Janne Lahtinen <janne.lahtinen@exove.com>
2023-04-20 08:20:05 +0300

    initial commit

---
+++ pages/coffee-store/[id].js
@@ -0,0 +1,122 @@
+import { useContext, useEffect, useState } from "react"
+import { useRouter } from "next/router"
+import Link from "next/link"
+import Head from "next/head"
+import Image from "next/image"
+import coffeeStoresData from '../../data/coffee-stores.json'
+import { fetchCoffeeStores } from '@/lib/coffee-stores'
+import cls from 'classnames'
+import styles from '../../styles/coffee-store.module.css'
+import { StoreContext } from "@/store/store-context"
+import { isEmpty } from "@/utils"
+
+export async function getStaticProps(staticProps) {
+  const params = staticProps.params
+  console.log('params', params)
+  const coffeeStores = await fetchCoffeeStores()
+  const findCoffeeStoreById = coffeeStores.find(coffeeStore => {
+    return coffeeStore.id === params.id // dynamic id
+  })
+  console.log({findCoffeeStoreById});
+  return {
+    props: {
+      coffeeStore: findCoffeeStoreById ? findCoffeeStoreById : {},
+    }, // will be passed to the page component as props
+  }
+}
+
+export async function getStaticPaths() {
+  const coffeeStores = await fetchCoffeeStores()
+  const paths = coffeeStores.map(coffeeStore => {
+    return {
+      params: {
+        id: coffeeStore.id.toString(),
+      }
+    }
+  })
+
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+const CoffeeStore = (initialProps) => {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <div>Loading..</div>
+  }
+
+  const id = router.query.id
+
+  const [coffeeStore, setCoffeeStore] = useState(initialProps.coffeeStore)
+
+  const {
+    state: {
+      coffeeStores,
+    }
+  } = useContext(StoreContext)
+
+  useEffect(() => {
+    if (isEmpty(initialProps.coffeeStore)) {
+      if (coffeeStores.length > 0) {
+        const findCoffeeStoreById = coffeeStores.find(coffeeStore => {
+          return coffeeStore.id === id // dynamic id
+        })
+        setCoffeeStore(findCoffeeStoreById)
+      }
+    }
+  }, [id])
+
+  const { address, name, imgUrl } = coffeeStore
+
+  const handleUpvoteButton = () => {
+    console.log('Handle upvote')
+  }
+
+  return (
+    <article>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">← Back to home</Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image 
+            src={imgUrl || 
+            'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'} 
+            width={600} 
+            height={360} 
+            className={styles.storeImg} 
+            alt={name || ''} />
+        </div>
+        <div className={cls('glass', styles.col2)}>
+          {address && (
+            <div className={styles.iconWrapper}>
+              <Image src='/static/icons/places.svg' width={24} height={24} alt='' />
+              <p className={styles.text}>{address}</p>
+            </div>
+          )}
+          {/* Neighborhood is no longer in the api
+            <div className={styles.iconWrapper}>
+            <Image src='/static/icons/nearMe.svg' width={24} height={24} alt='' />
+            <p className={styles.text}>{location.neighborhood}</p>
+          </div> */}
+          <div className={styles.iconWrapper}>
+            <Image src='/static/icons/star.svg' width={24} height={24} alt='' />
+            <p className={styles.text}>1</p>
+          </div>
+          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>Up vote!</button>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default CoffeeStore
